dbb6a3e8b3cea6785421c3322bdcb89f756d2a4d
QCzok <jan.czok@outlook.de>
2021-04-14 10:37:44 +0200

    changed config

--- src/constants/apiConstants.js
+++ src/constants/apiConstants.js
@@ -1,5 +1,5 @@
-// export const API_BASE_URL = 'https://soundtrackbox.herokuapp.com';
-export const API_BASE_URL = 'http://localhost:3002'
+export const API_BASE_URL = 'https://soundtrackbox.herokuapp.com';
+//export const API_BASE_URL = 'http://localhost:3002'
 export const ACCESS_TOKEN_NAME = 'login_access_token';
 export const CURRENT_SONG_NAME = null;
 export const CURRENT_SONG_ID = null;
